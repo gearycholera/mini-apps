4fd81922187f95983afb41dece5039683031e322
girikalra <girikalra@gmail.com>
2018-02-13 21:19:57 -0800

    added some styling. implemented unique id for each line.

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -2,17 +2,17 @@ var output = '';
 
 $(document).ready(function() {
   $('form').on('submit', function(e) {
-    var words = $('#text').val();
+    var words = $('#json').val();
     e.preventDefault();
     $.ajax({
       type: "POST",
       url: 'http://127.0.0.1:3000',
       data: {input: words},
       success: function(result) {
-        $("#csv-output").html("");
+        $("#csv").html("");
         output = JSON.parse(result);
-        output = output.replace(/\n/g, "<br />");
-        $("#csv-output").append(output);
+        //output = output.replace(/\n/g, "<br />");
+        $("#csv").html(output);
       },
       error: function() {
         console.log('error with post')
